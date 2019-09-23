c0225665184988468810e6b9d8b6c4db00db74fd
Ben Lin <522181941@qq.com>
2019-09-23 17:30:02 +0800

    fix bug

--- testFit.py
+++ testFit.py
@@ -1,25 +1,29 @@
 from os.path import join as pjoin
 import scipy.io as sio
 import torch
-from torchvision import transforms as tf
+from sklearn import preprocessing
 import numpy as np
 
 input_mat = sio.loadmat(pjoin('data','Real_MTMR_pos_4096.mat'))['input_mat']
 output_mat = sio.loadmat(pjoin('data','Real_MTMR_tor_4096.mat'))['output_mat']
 
-device =  torch.device('cpu')
-
-
+device = torch.device('cpu')
 H = 1000
 
-x = torch.from_numpy(input_mat.T).to(device)
-y = torch.from_numpy(output_mat.T).to(device)
+input_mat = input_mat.T
+output_mat = output_mat.T
+input_scaler = preprocessing.StandardScaler().fit(input_mat)
+output_scaler = preprocessing.StandardScaler().fit(output_mat)
+input_mat = input_scaler.transform(input_mat)
+output_mat = output_scaler.transform(output_mat)
+input_mat = input_mat[:, :-1]
+output_mat = output_mat[:, :-1]
+
+
+x = torch.from_numpy(input_mat).to(device)
+y = torch.from_numpy(output_mat).to(device)
 x = x.float()
 y = y.float()
-
-norm_ = tf.Normalize(0,1)
-x = norm_(x)
-y = norm_(y)
 
 (N_in, D_in) = x.shape
 (N_out, D_out) = y.shape
@@ -27,21 +31,21 @@ assert N_in == N_out
 
 model = torch.nn.Sequential(
             torch.nn.Linear(D_in, H),
-            torch.nn.ReLU(),
+            torch.nn.LeakyReLU(),
             torch.nn.Linear(H, D_out)).to(device)
 
-loss_fn = torch.nn.MSELoss(reduction='sum')
+loss_fn = torch.nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
-learning_rate = 1e-4
-for t in range(500):
+for t in range(3000):
     y_pred = model(x)
-
     loss = loss_fn(y_pred, y)
-    print(t, loss.item())
-    model.zero_grad()
-    loss.backward()
+    optimizer.zero_grad()  # clear gradients for next train
+    loss.backward()  # backpropagation, compute gradients
+    optimizer.step()  # apply gradients
+    loss_val = loss.item()
+    print('Epoch', t, ': Loss is ', loss_val)
 
-    with torch.no_grad():
-        for param in model.parameters():
-            param.data -= learning_rate * param.grad
+    if loss_val<1e-4:
+        break
 
