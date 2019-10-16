35576ff5d98149f579d8a1e090355c45ee51d502
Ben Lin <522181941@qq.com>
2019-10-16 18:36:10 +0800

    finish acrobot experient

--- simulator/get_RMS_result.m
+++ simulator/get_RMS_result.m
@@ -1,4 +1,5 @@
-net_list = {'ReLuNet.mat','SigmoidNet.mat','SinNet.mat','Lagrangian_SinNet.mat'}
+net_list = {'ReLuNet.mat','SigmoidNet.mat','SinNet.mat','Lagrangian_SinNet.mat','dynamic_model.mat'}
+legend_list = {'ReLu Net','Sigmoid Net','Sin Net','Lagrangian-Sin Net', 'Dynamic Model'}
 N_arr = [5,8,15];
 std_arr = [1, 5, 9];
 
@@ -13,6 +14,39 @@ for j=1:size(std_arr,2)
         end
         result_list{end+1} = result_mat;
     end
+end
+
+plot_rms(1, 1, legend_list,N_arr, result_list(1:3))
+plot_rms(2, 1, legend_list,N_arr, result_list(4:6))
+plot_rms(3, 1, legend_list,N_arr, result_list(7:9))
+plot_rms(4, 2, legend_list,N_arr, result_list(1:3))
+plot_rms(5, 2, legend_list,N_arr, result_list(4:6))
+plot_rms(6, 2, legend_list,N_arr, result_list(7:9))
+
+function plot_rms(plt_idx, jnt_no, legend_list,N_arr, result_list)
+    linewidth = 3;
+    mksize =12;
+    net_result_list ={};
+    for j=1:size(result_list{1},2)
+        result_mat = []
+        for i=1:numel(result_list)
+            result_mat = [result_mat, result_list{i}(:,j)]
+        end
+        net_result_list{end+1} = result_mat;
+    end
+    figure(plt_idx)
+    hold on
+    fntsize = 20;
+    p1 = plot(N_arr, net_result_list{1}(jnt_no,:),'-x','LineWidth',linewidth,'MarkerSize',mksize);
+    p2 = plot(N_arr, net_result_list{2}(jnt_no,:),'-x','LineWidth',linewidth,'MarkerSize',mksize);
+    p3 = plot(N_arr, net_result_list{3}(jnt_no,:),'-x','LineWidth',linewidth,'MarkerSize',mksize);
+    p4 = plot(N_arr, net_result_list{4}(jnt_no,:),'-x','LineWidth',linewidth,'MarkerSize',mksize);
+    p5 = plot(N_arr, net_result_list{5}(jnt_no,:),'-x','LineWidth',linewidth,'MarkerSize',mksize);
+    hold off
+    set(gca,'FontSize',fntsize);
+    legend([p1,p2,p3,p4, p5], legend_list,'location','best');
+    xlabel('$N$','interpreter','latex','FontSize',fntsize)
+    ylabel(sprintf("$\\epsilon_{RMS_%d} (\\%%)$", jnt_no),'interpreter','latex','FontSize',fntsize)
 end
 
 
--- simulator/train_dynamic_model.m
+++ simulator/train_dynamic_model.m
@@ -1,31 +1,42 @@
-load('../data/Acrobot_sim_25/Acrobot_sim_25.mat')
-
-R_mat = [];
-tau_vec = [];
-for i = 1:size(input_mat,1)
-    q1 = input_mat(i,1);
-    q2 = input_mat(i,2);
-    tau_vec = [tau_vec;output_mat(i,1);output_mat(i,2)];
-    R_mat = [R_mat;Regressor(q1,q2)];
+N_arr = [5, 8, 15];
+std_arr = [1, 5, 9];
+for j=1:size(std_arr,2)
+for i=1:size(N_arr,2)
+    train_model(N_arr(i), std_arr(j))
+end
 end
 
- beta_vec = pinv(R_mat)*tau_vec;
+function train_model(N, std)
+    train_file = sprintf("N%d_std%d\\data\\N%d_std%d.mat", N, std, N, std);
+    test_file = sprintf("N%d_std%d\\data\\N%d_std%d.mat", 34, std, 34, std);
+    load(train_file)
+    R_mat = [];
+    tau_vec = [];
+    for i = 1:size(input_mat,1)
+        q1 = input_mat(i,1);
+        q2 = input_mat(i,2);
+        tau_vec = [tau_vec;output_mat(i,1);output_mat(i,2)];
+        R_mat = [R_mat;Regressor(q1,q2)];
+    end
+
+     beta_vec = pinv(R_mat)*tau_vec;
 
 
-train_input_mat = input_mat;
-train_output_mat = output_mat;
+    train_input_mat = input_mat;
+    train_output_mat = output_mat;
 
-load('../data/Acrobot_sim_1156/Acrobot_sim_1156.mat')
-test_output_mat = [];
-for i = 1:size(input_mat,1)
-    q1 = input_mat(i,1);
-    q2 = input_mat(i,2);
-    tau =Regressor(q1,q2)*beta_vec;
-    test_output_mat = [test_output_mat;tau.'];
+    load(test_file)
+    test_output_mat = [];
+    for i = 1:size(input_mat,1)
+        q1 = input_mat(i,1);
+        q2 = input_mat(i,2);
+        tau =Regressor(q1,q2)*beta_vec;
+        test_output_mat = [test_output_mat;tau.'];
+    end
+    test_input_mat = input_mat;
+
+    save(sprintf("N%d_std%d\\result\\dynamic_model.mat", N, std), 'train_input_mat', 'train_output_mat', 'test_input_mat', 'test_output_mat');
 end
-test_input_mat = input_mat;
-
-save('../figure/bp_train_8/result.mat')
 
 function R = Regressor(q1,q2)
     s1 =sin(q1);
